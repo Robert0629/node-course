922fc567103ae13ca8510e4ed535a062a2760d3a
Robert <rl.84@icloud.com>
2022-05-14 15:12:53 +0800

    add callback.js

--- sum.js
+++ sum.js
@@ -1,16 +1,16 @@
 // 回傳 1 + 2 + ... + n 的結果
 
 function sum(n) {
-    let total = 0;
-   for (let i = 1; i <= n; i++){
-        total  += i;
-        }
-//    console.log(total);
-        return total;
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
   }
- 
- console.log(sum(1)); // 1
-   
-  console.log(sum(2)); // 3
-  console.log(sum(10)); // 55
-  console.log(sum(100)); // 5050
+  //    console.log(total);
+  return total;
+}
+
+console.log(sum(1)); // 1
+
+console.log(sum(2)); // 3
+console.log(sum(10)); // 55
+console.log(sum(100)); // 5050
