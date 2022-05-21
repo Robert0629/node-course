b65c9c1bc985a27c4f26b1e49f2b57771946c7c6
Robert <rl.84@icloud.com>
2022-05-21 11:38:39 +0800

    update

---
+++ crawler/crawler4.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2/promise');
+
+(async () => {
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'admin',
+    password: '12345',
+    database: 'node_course',
+  });
+
+  let [data, fields] = await connection.execute('SELECT * FROM stocks');
+  console.log(data);
+
+  connection.end();
+})();
